cc2bdbd11bf78c8c531e6933f6e54651afdadb06
coders-shelf <60462196+coders-shelf@users.noreply.github.com>
2021-10-09 18:46:36 +0900

    テーマの切り替え実装

--- src/App.js
+++ src/App.js
@@ -1,21 +1,74 @@
+import { useState, useEffect, useMemo } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Button from '@mui/material/Button';
 import CustomAppBar from './components/common/CustomAppBar';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+import ColorModeContext from './context/ColorModeContext';
+import Typography from '@mui/material/Typography';
+import { blueGrey, brown, grey } from '@mui/material/colors';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const App = () => {
+  const [mode, setMode] = useState('light');
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  useEffect(() => {
+    if (prefersDarkMode) {
+      setMode('dark');
+    } else {
+      setMode('light');
+    }
+  }, [prefersDarkMode]);
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          ...(mode === 'light'
+            ? {
+                primary: blueGrey,
+                divider: blueGrey[200],
+                text: {
+                  primary: grey[900],
+                  secondary: grey[800],
+                },
+              }
+            : {
+                primary: brown,
+                divider: brown[700],
+                background: {
+                  default: brown[900],
+                  paper: brown[900],
+                },
+                text: {
+                  primary: '#fff',
+                  secondary: brown[500],
+                },
+              }),
+        },
+      }),
+    [mode]
+  );
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    []
+  );
+
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <CustomAppBar />
-      <Button variant='contained'>Hello World</Button>
-    </ThemeProvider>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <CustomAppBar />
+        <Button variant='contained'>Hello World</Button>
+        <Typography variant='body1'>Hello MUI</Typography>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
 
--- src/components/common/CustomAppBar.js
+++ src/components/common/CustomAppBar.js
@@ -1,11 +1,14 @@
+import { useContext } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import InvertColorsIcon from '@mui/icons-material/InvertColors';
+import ColorModeContext from '../../context/ColorModeContext';
 
 const CustomAppBar = () => {
+  const colorMode = useContext(ColorModeContext);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='static'>
@@ -13,7 +16,7 @@ const CustomAppBar = () => {
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
             MUI v5でテーマをカスタマイズする
           </Typography>
-          <IconButton color='inherit'>
+          <IconButton color='inherit' onClick={colorMode.toggleColorMode}>
             <InvertColorsIcon />
           </IconButton>
         </Toolbar>
